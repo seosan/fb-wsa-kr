7ed2a51f28215bbd0382161fae9edae1f4f24859
seosan <lithrome@gmail.com>
2014-02-11 18:08:29 +0900

    yes

--- web.js
+++ web.js
@@ -1,15 +1,17 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var FB = require("fb");
 var app = express();
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.sendfile('/index.html');
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log(port);
+	console.log("Listening on " + port);
 });
