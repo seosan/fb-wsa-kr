e1f955021bf1b512a7c42297d0399fc31c5d2d96
seosan <lithrome@gmail.com>
2014-02-12 17:30:13 +0900

    good

--- web.js
+++ web.js
@@ -34,10 +34,11 @@ io.sockets.on('connection', function(socket) {
 	//----통신 시작----
 
 	//욕설
-	var yok =
+	var yokdb =
 	[
 		'Fuck', 'Shit', '뻨', '쒵', '쓑', '쒯', '개새끼', '개시끼', '개세끼', '개색기', '개색끼', '개객끼', '개객기', '개같은', '개놈', '개년', '개자식', '씨발', '씨벌', '씨불', '씹할', '씨팔', '씨벨', '씨부랄', '씨부럴', '씨팔', '씨펄', 'ㅅㅂ', 'ㅆㅂ', '씹창', '씹년', '씹놈', '좆', '존나', 'ㅈㄴ', '미친놈', '미친년', '미친새끼', '엄창', '앰창', '니미', '병신', '븅신', '븅딱', '퓽신', '비융신', '피융신', 'ㅄ', 'ㅂㅅ', '지랄', '닥쳐', '盧?', '젠장', '빌어먹을', '빌어쳐먹을', '빌어처먹을', '제기랄'
 	];
+	var yok = new Array();
 
 	for (kind in yok) {
 		yok[kind] = 0;
@@ -47,7 +48,7 @@ io.sockets.on('connection', function(socket) {
 	function search(string, pos, kind) {
 		if((yokpos = string.indexOf(kind, pos)) != -1) {
 			yok[kind]++;
-			console.log(kind); console.log(kind);console.log(kind);console.log(kind);console.log(kind);
+			
 			search(string, yokpos + kind.length, kind);
 		}
 	}
