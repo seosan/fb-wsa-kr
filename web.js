32508f9ed81264342643e4a0e10545a5932e7bbc
seosan <lithrome@gmail.com>
2014-02-12 23:20:02 +0900

    css

--- web.js
+++ web.js
@@ -28,6 +28,9 @@ app.use(logfmt.requestLogger());
 app.get('/', function(req, res) {
   res.sendfile('/usr/index.html', {root:__dirname});
 });
+app.get('/style', function(req, res) {
+  res.sendfile('/usr/style.css', {root:__dirname});
+});
 var port = Number(process.env.PORT || 5000);
 server.listen(port, function() {
 	console.log(port);
