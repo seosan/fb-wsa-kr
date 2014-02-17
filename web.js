4c8e12b77881365d898f7135242821409411b308
seosan <lithrome@gmail.com>
2014-02-17 17:36:12 +0900

    a

--- web.js
+++ web.js
@@ -66,7 +66,7 @@ ios.sockets.on('connection', function(socket) {
 		re = new RegExp(chodb[index], 'gi');
 		var matching = string.match(re);
 		cho[index]+=matching ? matching.length : 0;
-		string.replace(re, '');
+		//string.replace(re, '');
 	}
 	socket.on('custom', function (cusdb) {
 		for(var i=0; cusdb[i]; i++)
