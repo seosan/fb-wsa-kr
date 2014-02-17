27515a083b81ee97228158916dec642164afca60
seosan <lithrome@gmail.com>
2014-02-17 18:43:35 +0900

    a

--- web.js
+++ web.js
@@ -56,12 +56,13 @@ ios.sockets.on('connection', function(socket) {
 	for (index in chodb) {
 		cho[index] = 0;
 	}
-
+	var string;
 	var chopos;
-	function search1(string, pos, index) {
+	function search1(pos, index) {
 		if((chopos = string.indexOf(chodb[index], pos) ) != -1) {
 			cho[index]++;
-			search1(string, chopos+(chodb[index].length), index);
+			string = string.substring(0, chopos).concat(string.substring(chopos));
+			search1(chopos, index);
 		}
 		
 	}
@@ -72,9 +73,10 @@ ios.sockets.on('connection', function(socket) {
 			cho[index] = 0;
 		}
 	});
-	socket.on('toserver', function (string) {
+	socket.on('toserver', function (data) {
+		string = data;
 		for (index in chodb) {
-			search1(string, 0, index);
+			search1(0, index);
 			/*var re = new RegExp(chodb[index], 'gi');
 			var matching = string.match(re);
 			cho[index]+=matching ? matching.length : 0;
